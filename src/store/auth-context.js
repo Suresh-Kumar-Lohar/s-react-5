e0edb9d0cb3e03caf4395ffce000ed597077b0a6
Suresh-Kumar-Lohar <sureshlohar468@gmail.com>
2022-11-10 11:50:00 +0530

    style-debug task-10

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {}, // just for ide auto completion
+  onLogin: (email, password) => {},
 })
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('isLoggedIn')
+
+    if (storedUser === '1') {
+      setIsLoggedIn(true)
+    }
+  }, [])
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn')
+    setIsLoggedIn(false)
+  }
+
+  const loginHandler = () => {
+    localStorage.setItem('isLoggedIn', '1')
+    setIsLoggedIn(true)
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  )
+}
 
 export default AuthContext
